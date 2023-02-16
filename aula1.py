166c52c4cf98a42238dae29bcb0e6a24246fea3a
Dario da Silva Melo <dariosilvamelo@gmail.com>
2023-02-16 18:44:14 -0300

    Commit no vsconde

--- aula1.py
+++ aula1.py
@@ -6,7 +6,7 @@ import re
 # findall search sub
 # compile
 
-string = 'Este é um teste de expressões teste regulares.'
+string = 'expressões teste regulares.'
 print(re.search(r'teste', string))
 print(re.findall(r'teste', string))
 print(re.sub(r'teste', 'ABCD', string))
